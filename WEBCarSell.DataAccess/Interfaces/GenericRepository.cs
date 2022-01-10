aa69d89018137e09b26fe47ae43695199aff3274
vitalii.teteria <vitalii.teteria@nure.ua>
2022-01-10 14:44:44 +0200

    fix models

---
+++ WEBCarSell.DataAccess/Interfaces/GenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEBCarSell.DataAccess.Interfaces
+{
+    //class GenericRepository : IRepository
+    //{
+    //    private readonly WEBCarSellDBContext _dbContext;
+
+    //    public GenericRepository(WEBCarSellDBContext dbContext)
+    //    {
+    //        _dbContext = dbContext;
+    //    }
+    //}
+}
