87e48cd03b27245feac9d8d554da010a76b4243e
vitalii_teteria <teterya.vitalya@gmail.com>
2022-03-27 11:42:39 +0300

    modified extensions

--- WEBCarSell.BusinessLogic/DTO/ClientDto.cs
+++ WEBCarSell.BusinessLogic/DTO/ClientDto.cs
@@ -8,7 +8,7 @@ namespace WEBCarSell.BusinessLogic.DTO
 {
     public class ClientDto
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
 
         public string Name { get; set; }
 
--- WEBCarSell.BusinessLogic/DTO/ModelDto.cs
+++ WEBCarSell.BusinessLogic/DTO/ModelDto.cs
@@ -9,7 +9,7 @@ namespace WEBCarSell.BusinessLogic.DTO
 {
     public class ModelDto
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
 
         public string Name { get; set; }
 
